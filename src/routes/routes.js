03765ab1c7cae3322a280d73d1a6df3a74d1be85
Keian Anthony <KeianAnthony1@gmail.com>
2023-01-22 18:20:57 -0800

    send confirmation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,9 @@ router.signup = async (req, res) => {
     try {
         req.body.password = await bcrypt.hash(req.body.password, 10);
         let newUser = await new userModel(req.body);
+        // Sending created user, should I send WHOLE group here?
+        // ALSO -- TODO: Need to think about how groups are connected in the DB
+        res.status(200).send(newUser);
     } catch (e) {
         console.error(e.message);
         res.status(500).send("Invalid new user");
